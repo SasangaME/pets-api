fc3250322eb19e50f3bf69e12c6febd58747fb03
Sasanga Edirisinghe <madhumaledirisinghe@gmail.com>
2024-12-14 22:57:39 +0530

    nestjs configuration

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
+
+  getHealthCheck(): HealthCheckDto {
+    return {
+      title: this.configService.get<string>('TITLE'),
+      version: this.configService.get<string>('VERSION'),
+      env: this.configService.get<string>('ENV'),
+    };
+  }
+}
+
+export interface HealthCheckDto {
+  title: string;
+  version: string;
+  env: string;
 }
